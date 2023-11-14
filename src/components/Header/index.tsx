80bb2b56dfa8f3458d0f40faaf1091e1f1ca431c
Kenform <kenform@bk.ru>
2023-11-14 21:24:27 +0700

    recommit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import Burger from '../base/Burger';
+import Button from '../base/Button';
+import Search from '../Search';
+
 const Header: React.FC = () => {
+	
 	return (
-		
 		<header className='header'>
 			<div className='header__container'>
+				<a href='/' className='header__logo '>
+					<div className='logo-icon'>
+						<img src='img/logo/logo-icon.svg' alt='Logo BitList' />
+					</div>
+					<div className='logo-text'>
+						<img src='img/logo/logo-text.svg' alt='BitList' />
+					</div>
+					<div className='line'>
+						<img src='img/icons/base/line.svg' alt='' />
+					</div>
+				</a>
+
 				<div className='header__menu menu'>
-					<button type='button' className='menu__icon icon-menu'>
-						<span></span>
-					</button>
+					<Burger />
+					<Search/>
+					
 					<nav className='menu__body'>
 						<ul className='menu__list'>
 							<li className='menu__item'>
@@ -17,11 +33,33 @@ const Header: React.FC = () => {
 							</li>
 							<li className='menu__item'>
 								<a href='#' className='menu__link'>
-									Trade
+									P2P
+								</a>
+							</li>
+							<li className='menu__item'>
+								<a href='#' className='menu__link'>
+									Partners
+								</a>
+							</li>
+							<li className='menu__item'>
+								<a href='#' className='menu__link'>
+									Mining
+								</a>
+							</li>
+							<li className='menu__item'>
+								<a href='#' className='menu__link'>
+									Academy
 								</a>
 							</li>
 						</ul>
 					</nav>
+					<div className='line'>
+						<img src='img/icons/base/line.svg' alt='' />
+					</div>
+					<div className='header__actions'>
+						<Button modifier='gray' text='Register' />
+						<Button modifier='blue' text='Log in' />
+					</div>
 				</div>
 			</div>
 		</header>
