7c7b7f7abbb19b9eb2f19e52a766f0d291ef8200
Kenform <kenform@bk.ru>
2023-11-29 21:49:45 +0700

    made the registration section

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
 						<img src='img/lights/Intro/2.svg' alt='' />
 					</div>
 				</div>
-				<div className='intro __container'>
+				<div className='intro__container'>
 					<div className='intro__content '>
 						<div className='intro__body '>
 							<div className='intro__button'>
@@ -37,17 +37,33 @@ function App() {
 							<img src='img/graph.svg' alt='graph crypto transactions' />
 						</div>
 					</div>
-					<Email />
-
-					{/* <div className='intro__light_1'>
-							<img src='img/lights/Intro/1.svg' alt='' />
-						</div>
-						<div className='intro__light_2'>
-							<img src='img/lights/Intro/2.svg' alt='' />
-						</div> */}
+					<Email modifier='intro' />
 				</div>
 			</section>
-			
+
+			<section className='page__register section register'>
+				<div className='main-block__icons icons-main-block'>
+					<div className='icons-main-block__item icons-main-block__item_3'>
+						<img src='img/lights/Email/1.png' alt='' />
+					</div>
+					<div className='icons-main-block__item icons-main-block__item_4'>
+						<img src='img/lights/Email/2.png' alt='' />
+					</div>
+					<div className='icons-main-block__item icons-main-block__item_5'>
+						<img src='img/lights/Email/3.png' alt='' />
+					</div>
+				</div>
+				<div className='register__container'>
+					<div className='register__content'>
+						<div className='register__body'>
+							<div className='register__text title --48'>
+								<p>Register your account now and start to trade</p>
+							</div>
+							<Email modifier='' />
+						</div>
+					</div>
+				</div>
+			</section>
 			<Footer />
 		</div>
 	);
--- src/components/base/Email/index.tsx
+++ src/components/base/Email/index.tsx
@@ -1,15 +1,19 @@
 import Button from '../Button';
 
 import "./style.scss";
-const Email:React.FC = () => {
+
+type typeEmail ={
+	modifier:string;
+}
+const Email:React.FC<typeEmail> = ({modifier}) => {
 	return (
-		<div className='intro__email email-intro'>
-			<form action='#' className='email-intro__item'>
-				<div className='email-intro__icon '>
+		<div className={`email ${modifier}`}>
+			<form action='#' className='email__item'>
+				<div className='email__icon '>
 					<img src='img/icons/base/email.svg' alt='' />
 				</div>
-				<input placeholder='Example@gmail.com' type='email' className='email-intro__input' />
-				<div className='email-intro__button'>
+				<input placeholder='Example@gmail.com' type='email' className='email__input' />
+				<div className='email__button'>
 					<Button modifier='blue' text='Sign in' />
 				</div>
 			</form>
